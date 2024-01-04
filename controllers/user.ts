932d537cdcf87c09464f9e7612d52d3af7d1c9ce
autroshot <autroshot@gmail.com>
2024-01-04 23:58:53 +0900

    joinController 일부 구현

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,7 +1,21 @@
 import { RequestHandler } from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import { matchedData } from 'express-validator';
+import pool from '../maria-db';
 
-const join: RequestHandler = (req, res) => {
-    res.send('join');
-};
+const join: RequestHandler = expressAsyncHandler(async (req, res) => {
+    const { email, password } = matchedData(req) as {
+        email: string;
+        password: string;
+    };
+
+    const result = await pool.execute(
+        'INSERT INTO `programmers_bookstore`.`users` (`email`, `password`) VALUES (?, ?)',
+        [email, password]
+    );
+    console.log(result);
+
+    res.status(201).end();
+});
 
 export { join };
