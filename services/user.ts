d3d2e3bfa32c45b2c6b9442dd1e2817faa751466
autroshot <autroshot@gmail.com>
2024-01-05 15:39:02 +0900

    리팩터: DBErrorWrapper를 인라인으로 사용

--- services/user.ts
+++ services/user.ts
@@ -7,28 +7,29 @@ interface createForm {
     password: string;
 }
 
-async function create({ email, password }: createForm): Promise<void> {
-    const sql =
-        'INSERT INTO `users` (`email`, `password`) VALUES (:email, :password)';
-    const values = { email, password };
-    const result = await pool.execute(sql, values);
-    console.log(result);
-}
+const create = DBErrorWrapper(
+    async ({ email, password }: createForm): Promise<void> => {
+        const sql =
+            'INSERT INTO `users` (`email`, `password`) VALUES (:email, :password)';
+        const values = { email, password };
+        const result = await pool.execute(sql, values);
+        console.log(result);
+    }
+);
 
-async function update({
-    email,
-    password,
-}: createForm): Promise<[ResultSetHeader, FieldPacket[]]> {
-    const sql =
-        'UPDATE `users` SET `password` = :password WHERE (`email` = :email)';
-    const values = { email, password };
-    const result = await pool.execute<ResultSetHeader>(sql, values);
-    console.log(result);
+const update = DBErrorWrapper(
+    async ({
+        email,
+        password,
+    }: createForm): Promise<[ResultSetHeader, FieldPacket[]]> => {
+        const sql =
+            'UPDATE `users` SET `password` = :password WHERE (`email` = :email)';
+        const values = { email, password };
+        const result = await pool.execute<ResultSetHeader>(sql, values);
+        console.log(result);
 
-    return result;
-}
+        return result;
+    }
+);
 
-const wrappedCreate = DBErrorWrapper(create);
-const wrappedUpdate = DBErrorWrapper(update);
-
-export { wrappedCreate as create, wrappedUpdate as update };
+export { create, update };
