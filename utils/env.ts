db1e5117516cf1af8323fb37c365b9ac8a610296
autroshot <autroshot@gmail.com>
2024-01-07 19:09:30 +0900

    getEnv 함수 구현

---
+++ utils/env.ts
@@ -0,0 +1,10 @@
+function getEnv(key: string): string {
+    const value = process.env[key];
+
+    if (value === undefined) {
+        throw Error(`${key}에 대응되는 환경 변수 값이 없습니다.`);
+    }
+    return value;
+}
+
+export { getEnv };
