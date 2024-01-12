c591a84bfc319af32589c7a8addea16b1277f38f
autroshot <autroshot@gmail.com>
2024-01-12 15:53:11 +0900

    범주별 도서 목록 API에 id 유효성 검사 구현

--- routers/category.ts
+++ routers/category.ts
@@ -2,11 +2,19 @@ import {
     findManyBooks as findManyBooksController,
     findMany as findManyController,
 } from '@controllers/category';
+import { validationResultHandler } from '@middlewares/request-handlers';
+import { id as idSchema } from '@validators/book';
 import express from 'express';
+import { checkSchema } from 'express-validator';
 
 const router = express.Router();
 
 router.get('/', findManyController);
-router.get('/:id/books', findManyBooksController);
+router.get(
+    '/:id/books',
+    checkSchema(idSchema, ['params']),
+    validationResultHandler,
+    findManyBooksController
+);
 
 export default router;
