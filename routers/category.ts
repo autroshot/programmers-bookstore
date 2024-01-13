c70d177e9130f201dd86e1e22b655c26ef38f769
autroshot <autroshot@gmail.com>
2024-01-14 00:18:10 +0900

    페이지네이션 쿼리스트링에 대한 유효성 검사 구현하고 해당 라우터에 연결

--- routers/category.ts
+++ routers/category.ts
@@ -4,6 +4,7 @@ import {
 } from '@controllers/category';
 import { validationResultHandler } from '@middlewares/request-handlers';
 import { id as idSchema } from '@validators/book';
+import { pagination as paginationSchema } from '@validators/pagination';
 import express from 'express';
 import { checkSchema } from 'express-validator';
 
@@ -13,6 +14,7 @@ router.get('/', findManyController);
 router.get(
     '/:id/books',
     checkSchema(idSchema, ['params']),
+    checkSchema(paginationSchema, ['query']),
     validationResultHandler,
     findManyBooksByCategoryController
 );
