21a41ba5701fd988d488a1bf1fd7d2a7d012c58b
autroshot <autroshot@gmail.com>
2024-01-14 03:02:06 +0900

    리팩터: 페이징의 유효성 검사 스키마를 기본 내보내기로 변경

--- routers/book.ts
+++ routers/book.ts
@@ -4,7 +4,7 @@ import {
 } from '@controllers/book';
 import { validationResultHandler } from '@middlewares/request-handlers';
 import idSchema from '@validators/id';
-import { pagination as paginationSchema } from '@validators/pagination';
+import paginationSchema from '@validators/pagination';
 import express from 'express';
 import { checkSchema } from 'express-validator';
 
