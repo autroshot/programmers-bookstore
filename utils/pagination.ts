8433dfa6f9afc0713c5dd629c053abbd02b98470
autroshot <autroshot@gmail.com>
2024-01-14 10:32:22 +0900

    toDBPagination 함수에서 매개변수의 undefined 제거

--- utils/pagination.ts
+++ utils/pagination.ts
@@ -1,12 +1,8 @@
 import type { Pagination } from '@services/book';
 
-function toDBPagination(
-    page: number | undefined,
-    limit: number | undefined
-): Pagination | undefined {
-    if (typeof page === 'undefined' || typeof limit === 'undefined')
-        return undefined;
+function toDBPagination(page: number, limit: number): Pagination {
     const offset = (page - 1) * limit;
+
     return { offset, limit };
 }
 
