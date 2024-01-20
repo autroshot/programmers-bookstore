04d48715d975bb5f1bc9e54b3ce020eb65dfdb21
autroshot <autroshot@gmail.com>
2024-01-21 04:19:17 +0900

    리팩터: 도서 목록에 대한 유효성 검사를 라우터에서 컨트롤러로 옮김

--- controllers/book.ts
+++ controllers/book.ts
@@ -1,29 +1,39 @@
+import { validationResultHandler } from '@middlewares/request-handlers';
 import {
     count as countService,
     findMany as findManyService,
     findOne as findOneService,
 } from '@services/book';
 import { toDBPagination } from '@utils/pagination';
+import { isNew as isNewSchema } from '@validatorSchemas/book';
+import paginationSchema from '@validatorSchemas/pagination';
 import type { RequestHandler } from 'express';
 import expressAsyncHandler from 'express-async-handler';
-import { matchedData } from 'express-validator';
+import { checkSchema, matchedData } from 'express-validator';
 import { StatusCodes } from 'http-status-codes';
 
-const findMany: RequestHandler = expressAsyncHandler(async (req, res) => {
-    const { page, limit, isNew } = matchedData(req) as {
-        page: number;
-        limit: number;
-        isNew: boolean;
-    };
+type RequestHandlers = Array<Array<RequestHandler> | RequestHandler>;
 
-    const DBPagination = toDBPagination(page, limit);
-    const books = await findManyService(DBPagination, isNew);
-    const count = await countService();
+const findMany: RequestHandlers = [
+    checkSchema(paginationSchema, ['query']),
+    checkSchema(isNewSchema, ['query']),
+    validationResultHandler,
+    expressAsyncHandler(async (req, res) => {
+        const { page, limit, isNew } = matchedData(req) as {
+            page: number;
+            limit: number;
+            isNew: boolean;
+        };
 
-    const body = { books, totalPages: count };
-    res.status(StatusCodes.OK).json(body);
-    return;
-});
+        const DBPagination = toDBPagination(page, limit);
+        const books = await findManyService(DBPagination, isNew);
+        const count = await countService();
+
+        const body = { books, totalPages: count };
+        res.status(StatusCodes.OK).json(body);
+        return;
+    }),
+];
 
 const findOne: RequestHandler = expressAsyncHandler(async (req, res) => {
     const { id } = matchedData(req) as {
--- routers/book.ts
+++ routers/book.ts
@@ -1,23 +1,15 @@
 import {
-    findMany as findManyController,
+    findMany as findManyRequestHandlers,
     findOne as findOneController,
 } from '@controllers/book';
 import { validationResultHandler } from '@middlewares/request-handlers';
-import { isNew as isNewSchema } from '@validatorSchemas/book';
 import idSchema from '@validatorSchemas/id';
-import paginationSchema from '@validatorSchemas/pagination';
 import express from 'express';
 import { checkSchema } from 'express-validator';
 
 const router = express.Router();
 
-router.get(
-    '/',
-    checkSchema(paginationSchema, ['query']),
-    checkSchema(isNewSchema, ['query']),
-    validationResultHandler,
-    findManyController
-);
+router.get('/', ...findManyRequestHandlers);
 router.get(
     '/:id',
     checkSchema(idSchema, ['params']),
