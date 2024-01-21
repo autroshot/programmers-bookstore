7c58958868b57fdf05441f2426a04ff96da5cadc
autroshot <autroshot@gmail.com>
2024-01-21 13:04:21 +0900

    리팩터: 좋아요에 대한 유효성 검사를 라우터에서 컨트롤러로 옮기고 createRequestHandlers 사용

--- controllers/like.ts
+++ controllers/like.ts
@@ -1,52 +1,68 @@
+import { authenticate } from '@middlewares/request-handlers';
 import {
     create as createLikeService,
     findOne as findOneLikeService,
     remove as removeLikeService,
 } from '@services/like';
-import type { RequestHandler } from 'express';
-import expressAsyncHandler from 'express-async-handler';
-import { matchedData } from 'express-validator';
+import type { RequestHandlers } from '@utils/request-handler';
+import { createRequestHandlers } from '@utils/request-handler';
+import idSchema from '@validatorSchemas/id';
+import { checkSchema, matchedData } from 'express-validator';
 import { StatusCodes } from 'http-status-codes';
 
-const isExist: RequestHandler = expressAsyncHandler(async (req, res) => {
-    const { id: bookId } = matchedData(req) as {
-        id: number;
-    };
-    const userId = req.authenticatedId as number;
+const validations: RequestHandlers = [
+    authenticate,
+    checkSchema(idSchema, ['params']),
+];
 
-    const like = await findOneLikeService(userId, bookId);
-    const isLikeExist = like !== undefined;
+const isExist: RequestHandlers = createRequestHandlers({
+    validations,
+    requestHandler: async (req, res) => {
+        const { id: bookId } = matchedData(req) as {
+            id: number;
+        };
+        const userId = req.authenticatedId as number;
 
-    res.status(StatusCodes.OK).json(isLikeExist);
-    return;
-});
+        const like = await findOneLikeService(userId, bookId);
+        const isLikeExist = like !== undefined;
 
-const create: RequestHandler = expressAsyncHandler(async (req, res) => {
-    const { id: bookId } = matchedData(req) as {
-        id: number;
-    };
-    const userId = req.authenticatedId as number;
-
-    await createLikeService(userId, bookId);
-
-    res.status(StatusCodes.CREATED).end();
-    return;
-});
-
-const remove: RequestHandler = expressAsyncHandler(async (req, res) => {
-    const { id: bookId } = matchedData(req) as {
-        id: number;
-    };
-    const userId = req.authenticatedId as number;
-
-    const isSuccess = await removeLikeService(userId, bookId);
-
-    if (!isSuccess) {
-        res.status(StatusCodes.UNPROCESSABLE_ENTITY).end();
+        res.status(StatusCodes.OK).json(isLikeExist);
         return;
-    }
-    res.status(StatusCodes.NO_CONTENT).end();
-    return;
+    },
+});
+
+const create: RequestHandlers = createRequestHandlers({
+    validations,
+    requestHandler: async (req, res) => {
+        const { id: bookId } = matchedData(req) as {
+            id: number;
+        };
+        const userId = req.authenticatedId as number;
+
+        await createLikeService(userId, bookId);
+
+        res.status(StatusCodes.CREATED).end();
+        return;
+    },
+});
+
+const remove: RequestHandlers = createRequestHandlers({
+    validations,
+    requestHandler: async (req, res) => {
+        const { id: bookId } = matchedData(req) as {
+            id: number;
+        };
+        const userId = req.authenticatedId as number;
+
+        const isSuccess = await removeLikeService(userId, bookId);
+
+        if (!isSuccess) {
+            res.status(StatusCodes.UNPROCESSABLE_ENTITY).end();
+            return;
+        }
+        res.status(StatusCodes.NO_CONTENT).end();
+        return;
+    },
 });
 
 export { create, isExist, remove };
--- routers/like.ts
+++ routers/like.ts
@@ -1,28 +1,16 @@
 import {
-    create as createController,
-    isExist as isExistController,
-    remove as removeController,
+    create as createRequestHandlers,
+    isExist as isExistRequestHandlers,
+    remove as removeRequestHandlers,
 } from '@controllers/like';
-import {
-    authenticate,
-    validationResultHandler,
-} from '@middlewares/request-handlers';
-import idSchema from '@validatorSchemas/id';
 import express from 'express';
-import { checkSchema } from 'express-validator';
 
 const router = express.Router({ mergeParams: true });
 
-router.use(
-    authenticate,
-    checkSchema(idSchema, ['params']),
-    validationResultHandler
-);
-
 router
     .route('/')
-    .get(isExistController)
-    .post(createController)
-    .delete(removeController);
+    .get(...isExistRequestHandlers)
+    .post(...createRequestHandlers)
+    .delete(...removeRequestHandlers);
 
 export default router;
