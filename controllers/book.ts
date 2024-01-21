7070eb21efde790e2e1feb2dfa78a9b2dcf08399
autroshot <autroshot@gmail.com>
2024-01-21 11:15:37 +0900

    리팩터: 도서 컨트롤러에서 createRequestHandlers 함수 추출

--- controllers/book.ts
+++ controllers/book.ts
@@ -15,11 +15,12 @@ import { StatusCodes } from 'http-status-codes';
 
 type RequestHandlers = Array<Array<RequestHandler> | RequestHandler>;
 
-const findMany: RequestHandlers = [
-    checkSchema(paginationSchema, ['query']),
-    checkSchema(isNewSchema, ['query']),
-    validationResultHandler,
-    expressAsyncHandler(async (req, res) => {
+const findMany: RequestHandlers = createRequestHandlers({
+    validations: [
+        checkSchema(paginationSchema, ['query']),
+        checkSchema(isNewSchema, ['query']),
+    ],
+    requestHandler: async (req, res) => {
         const { page, limit, isNew } = matchedData(req) as {
             page: number;
             limit: number;
@@ -33,13 +34,12 @@ const findMany: RequestHandlers = [
         const body = { books, totalPages: count };
         res.status(StatusCodes.OK).json(body);
         return;
-    }),
-];
+    },
+});
 
-const findOne: RequestHandlers = [
-    checkSchema(idSchema, ['params']),
-    validationResultHandler,
-    expressAsyncHandler(async (req, res) => {
+const findOne: RequestHandlers = createRequestHandlers({
+    validations: [checkSchema(idSchema, ['params'])],
+    requestHandler: async (req, res) => {
         const { id } = matchedData(req) as {
             id: number;
         };
@@ -52,7 +52,32 @@ const findOne: RequestHandlers = [
         }
         res.status(StatusCodes.OK).json(book);
         return;
-    }),
-];
+    },
+});
+
+/**
+ * 라우터에 사용될 요청 처리기 배열을 만든다.
+ * @param requestHandler 가장 마지막에 실행될 요청 처리기 (비동기도 가능)
+ * @param validations 먼저 실행될 유효성 검사들
+ * @returns 요청 처리기 배열
+ */
+function createRequestHandlers({
+    requestHandler,
+    validations,
+}: Params): RequestHandlers {
+    if (validations === undefined) return [expressAsyncHandler(requestHandler)];
+    return [
+        ...validations,
+        validationResultHandler,
+        expressAsyncHandler(requestHandler),
+    ];
+}
+
+interface Params {
+    requestHandler: (
+        ...arg: Parameters<RequestHandler>
+    ) => void | Promise<void>;
+    validations?: RequestHandlers;
+}
 
 export { findMany, findOne };
