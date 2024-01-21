9d0341f49db16657522a517318782a2bacad1f80
autroshot <autroshot@gmail.com>
2024-01-21 14:19:33 +0900

    리팩터: createRequestHandlers 함수의 매개변수 형식 변경

--- utils/request-handler.ts
+++ utils/request-handler.ts
@@ -4,14 +4,14 @@ import expressAsyncHandler from 'express-async-handler';
 
 /**
  * 라우터에 사용될 요청 처리기 배열을 만든다.
+ * @param params 매개변수 객체
  * @param params.validations 먼저 실행될 유효성 검사들 (선택 사항)
  * @param params.requestHandler 마지막에 실행될 요청 처리기 (비동기도 가능)
  * @returns 요청 처리기 배열
  */
-function createRequestHandlers({
-    validations,
-    requestHandler,
-}: Params): RequestHandlers {
+function createRequestHandlers(params: Params): RequestHandlers {
+    const { validations, requestHandler } = params;
+
     if (validations === undefined) return [expressAsyncHandler(requestHandler)];
     return [
         ...validations,
